b18c5c30d5aae79e6353175387e616b9dec345c2
JoonHong-Kim <danielkim30433@gmail.com>
2023-03-09 19:19:11 +0900

    add dotenv

--- api/assignment/src/config/database/config.service.ts
+++ api/assignment/src/config/database/config.service.ts
@@ -13,7 +13,7 @@ export class PostgresConfigService implements TypeOrmOptionsFactory {
       password: this.configService.get<string>('DB_PASSWORD'),
       port: +this.configService.get<number>('DB_PORT'),
       host: this.configService.get<string>('DB_HOST'),
-      database: this.configService.get<string>('DB_SCHEMA'),
+      database: this.configService.get<string>('DB_NAME'),
       entities: this.configService.get<string[]>('ENTITY_PATH'),
       autoLoadEntities: true,
     };
