efbb48e7fe9a119e2873ef2651d41859f0b23d83
Joonhong Kim <30318926+JoonHong-Kim@users.noreply.github.com>
2023-03-18 14:10:21 +0900

    Revert "Assignment 6. Cloud Computing Service (#9)" (#10)
    This reverts commit afdf5bbf1d514b77d988ec4a7296982e75585908.

--- api/assignment/src/config/database/config.module.ts
+++
@@ -1,7 +0,0 @@
-import { Module } from '@nestjs/common';
-import { PostgresConfigService } from './config.service';
-
-@Module({
-  providers: [PostgresConfigService],
-})
-export class PostgresConfigModule {}
