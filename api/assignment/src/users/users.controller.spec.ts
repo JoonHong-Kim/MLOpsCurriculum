c6155bc96671bc57b6e6d7f9887df2fd9aaa0636
JoonHong-Kim <danielkim30433@gmail.com>
2023-03-18 14:07:06 +0900

    feat: user controller unit test

--- api/assignment/src/users/users.controller.spec.ts
+++ api/assignment/src/users/users.controller.spec.ts
@@ -4,7 +4,8 @@ import { UsersService } from './users.service';
 
 describe('UsersController', () => {
   let controller: UsersController;
-
+  let id: number = 0;
+  const mockUser: any = { name: 'Joonhong', age: 27 };
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UsersController],
@@ -16,5 +17,53 @@ describe('UsersController', () => {
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  describe('create', () => {
+    it('should return a user', () => {
+      expect(controller.create(mockUser)).toEqual({
+        id: id++,
+        ...mockUser,
+      });
+    });
+  });
+
+  describe('findAll', () => {
+    it('should return an array of users', () => {
+      controller.create(mockUser);
+      expect(controller.findAll()).toEqual([{ id: 0, ...mockUser }]);
+    });
+  });
+
+  describe('findOne', () => {
+    it('should return a user', () => {
+      controller.create(mockUser);
+      expect(controller.findOne('0')).toEqual({
+        id: 0,
+        ...mockUser,
+      });
+    });
+  });
+
+  describe('update', () => {
+    it('should return a user', () => {
+      controller.create(mockUser);
+      expect(controller.update('0', { name: 'eee', age: 22 })).toEqual({
+        id: 0,
+        name: 'eee',
+        age: 22,
+      });
+    });
+  });
+
+  describe('remove', () => {
+    it('should return a user', () => {
+      controller.create(mockUser);
+      expect(controller.remove('0')).toEqual({
+        id: 0,
+        ...mockUser,
+      });
+      expect(controller.findAll()).toEqual([]);
+    });
   });
 });
